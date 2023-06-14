720e53af86df225f14da85ef1342df4d76ea7620
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-06-14 20:55:48 +0200

    feat: trash icon

--- src/app/components/times/time-table/time-table.component.ts
+++ src/app/components/times/time-table/time-table.component.ts
@@ -1,18 +1,22 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MillisecondsToTimePipe } from '../../../pipes/milliseconds-to-time.pipe';
 import { DurationPipe } from '../../../pipes/duration.pipe';
 import { TimeEntryWithDuration } from '../../../services/time-tracking/time.entry';
 import { UnixDatePipe } from '../../../pipes/unix-date.pipe';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
     selector: 'kw-time-table',
     standalone: true,
-    imports: [CommonModule, MillisecondsToTimePipe, DurationPipe, UnixDatePipe],
+    imports: [CommonModule, MillisecondsToTimePipe, DurationPipe, UnixDatePipe, FontAwesomeModule],
     templateUrl: './time-table.component.html',
     styleUrls: ['./time-table.component.css'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TimeTableComponent {
     @Input({ required: true }) times: TimeEntryWithDuration[] = [];
+    @Output() trash = new EventEmitter<TimeEntryWithDuration>();
+    protected readonly faTrashAlt = faTrashAlt;
 }
