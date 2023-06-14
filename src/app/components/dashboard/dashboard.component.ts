e822531354ce1483c5c2b436a0107ce8d733ae41
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-06-14 23:24:41 +0200

    feat: very first mini chart test

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from '../presentation/card/card.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { TimeTable } from '../../services/time-tracking/time.table';
+import { DateTime } from 'luxon';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
     selector: 'kw-dashboard',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, CardComponent, NgxChartsModule],
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.css'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class DashboardComponent {}
+export default class DashboardComponent {
+    private readonly timeTable = inject(TimeTable);
+    protected chartData = toObservable(this.timeTable.groupByDay(0, DateTime.now().toMillis())).pipe(
+        map(data => {
+            return [
+                {
+                    name: 'Time',
+                    series: data.map(d => ({
+                        name: `${d.utcDate}`,
+                        value: d.duration.toMillis(),
+                    })),
+                },
+            ];
+        }),
+    );
+}
---
+++ src/d3types.d.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+declare module 'd3-scale' {
+    export interface ScaleLinear<T = any, T2 = any> {}
+    export interface ScaleBand<T = any> {}
+    export interface ScaleTime<T = any, T2 = any> {}
+    export interface ScalePoint<T = any> {}
+}
+declare module 'd3-shape' {
+    export interface DefaultArcObject {}
+    export interface CurveFactory {}
+}
+declare module 'd3-selection' {
+    export interface BaseType {}
+}
