697c7070b08b77692dca44c356d785f3c67a579a
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2023-06-24 10:14:17 +0200

    feat: virtual scrolling

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,14 +10,16 @@ import { CommonModule } from '@angular/common';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
+    @Input() fullSize = false;
     @Input() contentType: 'default' | 'chart' = 'default';
+
     get contentTypeClasses(): string {
         switch (this.contentType) {
             case 'chart':
                 return 'p-4 grid grid-cols-1';
 
             default:
-                return 'p-4 flex flex-col';
+                return ['flex flex-col', this.fullSize ? 'h-full' : 'p-4'].join(' ');
         }
     }
 }
--- src/app/components/times/time-table/time-table.component.ts
+++ src/app/components/times/time-table/time-table.component.ts
@@ -6,16 +6,29 @@ import { TimeEntryWithDuration } from '../../../services/time-tracking/time.mode
 import { UnixDatePipe } from '../../../pipes/unix-date.pipe';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import { CdkFixedSizeVirtualScroll, CdkVirtualForOf, CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { ScrollViewportProviderDirective } from './scroll-viewport-provider.directive';
 
 @Component({
     selector: 'kw-time-table',
     standalone: true,
-    imports: [CommonModule, MillisecondsToTimePipe, DurationPipe, UnixDatePipe, FontAwesomeModule],
+    imports: [
+        CommonModule,
+        MillisecondsToTimePipe,
+        DurationPipe,
+        UnixDatePipe,
+        FontAwesomeModule,
+        CdkVirtualScrollViewport,
+        CdkVirtualForOf,
+        CdkFixedSizeVirtualScroll,
+        ScrollViewportProviderDirective,
+    ],
     templateUrl: './time-table.component.html',
     styleUrls: ['./time-table.component.css'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TimeTableComponent {
+    @Input() useVirtualScroll = false;
     @Input({ required: true }) times: TimeEntryWithDuration[] = [];
     @Output() delete = new EventEmitter<TimeEntryWithDuration>();
     protected readonly faTrashAlt = faTrashAlt;
